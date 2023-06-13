1a99d5a5c846b4cb2a290150f0b3c5f477dc421e
Akula Vamsi <54858425+Vamsi12345-eng@users.noreply.github.com>
2023-06-14 01:44:58 +0530

    Add files via upload

--- Day_006/Sequence_detector.v
+++ Day_006/Sequence_detector.v
@@ -1,1 +1,81 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:27:45 06/14/2023 
+// Design Name: 
+// Module Name:    Sequence_detector 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Sequence_detector(in,out,rst,clk);
 
+input in;
+input clk;
+input rst;
+output reg out;
+
+parameter s0=2'b00,
+          s1=2'b01,
+			 s2=2'b10;
+
+reg [1:0]present_state,next_state;
+
+			 
+always@(posedge clk or negedge rst) begin
+if(!rst)
+present_state<=0;
+else 
+present_state<=next_state;
+end
+
+always@(present_state or in) begin
+case(present_state)
+s0: begin
+     if(in==1'b0) begin
+	  next_state=s0;
+	  out=0;
+	  end
+	  else begin
+	  next_state=s1;
+	  out=0;
+	  end
+    end
+s1: begin
+    if(in==1'b0) begin
+	 next_state=s2;
+	 out=0;
+	 end
+	 else begin
+	 next_state=s0;
+	 out=0;
+	 end
+    end
+s2: begin
+    if(in==1'b1) begin
+	 next_state=s0;
+	 out=1;
+	 end
+	 else begin
+	 next_state=s0;
+	 out=0;
+	 end
+    end
+default: next_state=s0;
+endcase
+
+end
+
+
+
+endmodule
---
+++ Day_006/Sequence_detector_test.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   01:08:00 06/14/2023
+// Design Name:   Sequence_detector
+// Module Name:   Sequence_detector_test.v
+// Project Name:  Day_006
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Sequence_detector
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module Sequence_detector_test_v;
+
+	// Inputs
+	reg in;
+	reg rst;
+	reg clk;
+
+	// Outputs
+	wire out;
+
+	// Instantiate the Unit Under Test (UUT)
+	Sequence_detector uut (
+		.in(in), 
+		.out(out), 
+		.rst(rst), 
+		.clk(clk)
+	);
+
+	initial begin
+		// Initialize Inputs
+		in = 0;
+		rst = 1;
+		clk = 0;
+
+	 
+	   #10
+		in=1;
+		
+		#10
+		in=0;
+		
+		#10
+		in=1;
+		
+
+	end
+      always #5 clk=~clk;
+endmodule
+
