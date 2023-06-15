bccf1586f7d87c5e02a985d7126fbc503428a782
Akula Vamsi <54858425+Vamsi12345-eng@users.noreply.github.com>
2023-06-15 19:18:49 +0530

    Add files via upload

--- Day_007/TB/Bit_4_UP_DOWN_COUNTER_test.v
+++ Day_007/TB/Bit_4_UP_DOWN_COUNTER_test.v
@@ -4,15 +4,15 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   10:48:31 06/15/2023
-// Design Name:   Bit_4_SISO
-// Module Name:   Bit_4_SISO_test.v
+// Create Date:   09:54:37 06/15/2023
+// Design Name:   Bit_4_UP_DOWN_COUNTER
+// Module Name:   Bit_4_UP_DOWN_COUNTER_test.v
 // Project Name:  day_007
 // Target Device:  
 // Tool versions:  
 // Description: 
 //
-// Verilog Test Fixture created by ISE for module: Bit_4_SISO
+// Verilog Test Fixture created by ISE for module: Bit_4_UP_DOWN_COUNTER
 //
 // Dependencies:
 // 
@@ -22,35 +22,40 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
-module Bit_4_SISO_test_v;
+module Bit_4_UP_DOWN_COUNTER_test_v;
 
 	// Inputs
-	reg in;
+
+	reg trig;
 	reg clk;
 	reg rst;
 
 	// Outputs
-	wire [3:0] out;
+	wire [3:0] count_out;
 
 	// Instantiate the Unit Under Test (UUT)
-	Bit_4_SISO uut (
-		.in(in), 
+	Bit_4_UP_DOWN_COUNTER uut (
+		
+		.trig(trig), 
 		.clk(clk), 
 		.rst(rst), 
-		.out(out)
+		.count_out(count_out)
 	);
 
 	initial begin
 		// Initialize Inputs
-		in = 0;
+		trig = 0;
 		clk = 0;
 		rst = 1;
 		
-		in=1;
-		
+		#10
+		trig=1'b1;
+		#80
+		trig=1'b0;
 		#50
 		$stop;
 
+      
 		
 
 	end
