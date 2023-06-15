55af63ecfa05fa9e40b7c915a2a0c850e5ecf49a
Akula Vamsi <54858425+Vamsi12345-eng@users.noreply.github.com>
2023-06-15 19:16:35 +0530

    Add files via upload

--- Day_007/Bit_4_UP_DOWN_COUNTER.v
+++ Day_007/Bit_4_UP_DOWN_COUNTER.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    10:36:35 06/15/2023 
+// Create Date:    08:56:46 06/15/2023 
 // Design Name: 
-// Module Name:    Bit_4_SISO 
+// Module Name:    Bit_4_UP_DOWN_COUNTER 
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,24 +18,36 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Bit_4_SISO(in,clk,rst,out);
+module Bit_4_UP_DOWN_COUNTER(trig,clk,rst,count_out);
 
-input in;
+
+input trig;
 input clk,rst;
-output reg [3:0] out=0;
+output reg [3:0] count_out=0;
 
 always@(posedge clk or negedge rst) begin
+
 if(!rst) begin
-out<=4'b0000;
+count_out=4'd0;
+end
+
+else if(trig==1'b1) begin
+if(count_out>=4'd0 && count_out<4'd15) begin
+count_out=count_out+1;
 end
 else begin
-out[0]<=in;
-out[1]<=out[0];
-out[2]<=out[1];
-out[3]<=out[2];
+count_out=4'd0;
+end
 end
 
+else if(trig==1'b0) begin
+if(count_out>4'd0 && count_out<=4'd15) begin
+count_out=count_out-1;
 end
-
+else begin
+count_out=4'd15;
+end
+end
+end
 
 endmodule
