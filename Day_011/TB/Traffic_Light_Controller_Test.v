5c5ec5890fbf031af09a1df2d65d7b2dbaa94a55
Akula Vamsi <54858425+Vamsi12345-eng@users.noreply.github.com>
2023-06-23 23:49:42 +0530

    Add files via upload

--- Day_011/TB/Traffic_Light_Controller_Test.v
+++ Day_011/TB/Traffic_Light_Controller_Test.v
@@ -4,15 +4,15 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   11:56:35 06/22/2023
-// Design Name:   Clock_Divider
-// Module Name:   Clock_Divider_Test.v
+// Create Date:   13:06:17 06/23/2023
+// Design Name:   Traffic_Light_Controller
+// Module Name:   Traffic_Light_Controller_Test.v
 // Project Name:  day_011
 // Target Device:  
 // Tool versions:  
 // Description: 
 //
-// Verilog Test Fixture created by ISE for module: Clock_Divider
+// Verilog Test Fixture created by ISE for module: Traffic_Light_Controller
 //
 // Dependencies:
 // 
@@ -22,28 +22,40 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
-module Clock_Divider_Test_v;
+module Traffic_Light_Controller_Test_v;
 
 	// Inputs
 	reg clk;
+	reg rst;
 
 	// Outputs
-	wire divided_clk;
+	wire [2:0] D1;
+	wire [2:0] D2;
+	wire [2:0] D3;
+	wire [2:0] D4;
 
 	// Instantiate the Unit Under Test (UUT)
-	Clock_Divider uut (
+	Traffic_Light_Controller uut (
+		.D1(D1), 
+		.D2(D2), 
+		.D3(D3), 
+		.D4(D4), 
 		.clk(clk), 
-		.divided_clk(divided_clk)
+		.rst(rst)
 	);
 
 	initial begin
 		// Initialize Inputs
 		clk = 0;
+		rst = 0;
+		
+		#10
+		rst=1;
+		
 
-		
-		
+
 
 	end
-      always #5 clk=~clk;
+     always #5 clk=~clk; 
 endmodule
 
