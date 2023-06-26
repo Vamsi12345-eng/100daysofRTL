2f5f428a2c6401fa13759a4e64c04d8748d90776
Akula Vamsi <54858425+Vamsi12345-eng@users.noreply.github.com>
2023-06-26 10:58:51 +0530

    Add files via upload

--- Day_013/TB/Booth_Multiplier_TEST.v
+++ Day_013/TB/Booth_Multiplier_TEST.v
@@ -1,1 +1,58 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   10:23:40 06/26/2023
+// Design Name:   Booth_Multiplier
+// Module Name:   Booth_Multiplier_TEST.v
+// Project Name:  day_013
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Booth_Multiplier
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module Booth_Multiplier_TEST_v;
+
+	// Inputs
+	reg [4:0] in1;
+	reg [4:0] in2;
+
+	// Outputs
+	wire [8:0] out;
+
+	// Instantiate the Unit Under Test (UUT)
+	Booth_Multiplier uut (
+		.in1(in1), 
+		.in2(in2), 
+		.out(out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		in1 = 5'b11000;
+		in2 = 5'b11001;
+		
+		#10
+		in1=5'b01011;
+		in2=5'b10011;
+		
+		#30
+		$stop;
+
+		
+
+	end
+      
+endmodule
 
