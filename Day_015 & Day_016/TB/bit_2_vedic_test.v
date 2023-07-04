30b9580ddffc5c67b3a0097734c2cbb34491e218
Akula Vamsi <54858425+Vamsi12345-eng@users.noreply.github.com>
2023-07-04 19:18:54 +0530

    Add files via upload

--- Day_015 & Day_016/TB/bit_2_vedic_test.v
+++ Day_015 & Day_016/TB/bit_2_vedic_test.v
@@ -4,15 +4,15 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   20:08:56 07/03/2023
-// Design Name:   bit_8_vedic
-// Module Name:   bit_8_vedic_test.v
+// Create Date:   19:32:27 07/03/2023
+// Design Name:   bit_2_vedic
+// Module Name:   bit_2_vedic_test.v
 // Project Name:  day_015&016
 // Target Device:  
 // Tool versions:  
 // Description: 
 //
-// Verilog Test Fixture created by ISE for module: bit_8_vedic
+// Verilog Test Fixture created by ISE for module: bit_2_vedic
 //
 // Dependencies:
 // 
@@ -22,17 +22,17 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
-module bit_8_vedic_test_v;
+module bit_2_vedic_test_v;
 
 	// Inputs
-	reg [7:0] in1;
-	reg [7:0] in2;
+	reg [1:0] in1;
+	reg [1:0] in2;
 
 	// Outputs
-	wire [15:0] out;
+	wire [3:0] out;
 
 	// Instantiate the Unit Under Test (UUT)
-	bit_8_vedic uut (
+	bit_2_vedic uut (
 		.in1(in1), 
 		.in2(in2), 
 		.out(out)
@@ -42,14 +42,16 @@ integer i;
 		// Initialize Inputs
 		in1 = 0;
 		in2 = 0;
-
-		for(i=0;i<=6;i=i+1) begin
+  
+      for(i=0;i<=3;i=i+1) begin
 		#10
 		in1=i;
-		in2=i+3;
+		in2=i;
 		end
+		
 		#10
 		$stop;
+		
 
 	end
       
