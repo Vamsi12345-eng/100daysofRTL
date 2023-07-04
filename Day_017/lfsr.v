15a50e3b93e9b883ed5e31cd223ea6d3637ecf3f
Akula Vamsi <54858425+Vamsi12345-eng@users.noreply.github.com>
2023-07-04 19:25:54 +0530

    Add files via upload

--- Day_017/lfsr.v
+++ Day_017/lfsr.v
@@ -1,1 +1,16 @@
+`timescale 1ns / 1ps
 
+module lfsr(clk,out);
+
+input clk;
+output reg [3:0] out=4'b1111;
+
+always@(posedge clk) begin
+
+out[0]<=out[3]^out[2];
+out[1]<=out[0];
+out[2]<=out[1];
+out[3]<=out[2];
+end
+
+endmodule
---
+++ Day_017/lfsr_test.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   18:47:17 07/04/2023
+// Design Name:   lfsr
+// Module Name:   lfsr_test.v
+// Project Name:  day_017
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: lfsr
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module lfsr_test_v;
+
+	// Inputs
+	reg clk;
+
+	// Outputs
+	wire [3:0] out;
+
+	// Instantiate the Unit Under Test (UUT)
+	lfsr uut (
+		.clk(clk), 
+		.out(out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+
+		#150
+		$stop;
+
+	end
+      always #5 clk=~clk;
+endmodule
+
