29e246df6a3f9eb5150b4f187e3c2f5be7d90175
Akula Vamsi <54858425+Vamsi12345-eng@users.noreply.github.com>
2023-07-06 11:37:25 +0530

    Add files via upload

--- Day_018/B2G_G2B_CONV.v
+++ Day_018/B2G_G2B_CONV.v
@@ -1,1 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:12:50 07/06/2023 
+// Design Name: 
+// Module Name:    B2G_G2B_CONV 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module B2G_G2B_CONV(chooser,in,out);
 
+input chooser;
+input [3:0] in;
+output reg [3:0] out;
+
+always@( * ) begin
+if(chooser==1'b1) begin // means we choose the binary to grey converter
+out[3]=in[3];
+out[2]=in[3]^in[2];
+out[1]=in[2]^in[1];
+out[0]=in[1]^in[0];
+end
+else if(chooser==1'b0) begin // means we choose the grey to binary converter
+out[3]=in[3];
+out[2]=out[3]^in[2];
+out[1]=out[2]^in[1];
+out[0]=out[1]^in[0];
+end
+end
+
+endmodule
---
+++ Day_018/TEST.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   11:19:06 07/06/2023
+// Design Name:   B2G_G2B_CONV
+// Module Name:   TEST.v
+// Project Name:  Day_018
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: B2G_G2B_CONV
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module TEST_v;
+
+	// Inputs
+	reg chooser;
+	reg [3:0] in;
+
+	// Outputs
+	wire [3:0] out;
+
+	// Instantiate the Unit Under Test (UUT)
+	B2G_G2B_CONV uut (
+		.chooser(chooser), 
+		.in(in), 
+		.out(out)
+	);
+integer i;
+initial begin
+
+
+
+for(i=0;i<=15;i=i+1) begin
+#10
+chooser=1'b1;
+in=i;
+
+end
+
+for(i=0;i<=15;i=i+1) begin
+#10
+chooser=1'b0;
+in=i;
+end
+
+
+
+#10
+$stop;
+
+end
+      
+endmodule
+
