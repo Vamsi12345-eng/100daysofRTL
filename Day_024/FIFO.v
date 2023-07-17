fee7531a77f8480d72e082706feef0b0ee42ec2b
Akula Vamsi <54858425+Vamsi12345-eng@users.noreply.github.com>
2023-07-17 13:53:55 +0530

    Add files via upload

--- Day_024/FIFO.v
+++ Day_024/FIFO.v
@@ -1,1 +1,64 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:35:12 07/17/2023 
+// Design Name: 
+// Module Name:    FIFO 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FIFO (
+  input wire clk,
+  input wire reset,
+  input wire write_enable,
+  input wire read_enable,
+  input wire [15:0] data_in,
+  output reg [15:0] data_out,
+  output reg empty = 1'b1,
+  output reg full = 1'b0
+);
 
+  reg [15:0] memory [31:0];
+  reg [4:0] write_ptr = 5'b0; // Initial value of write_ptr is 0
+  reg [4:0] read_ptr = 5'b0; // Initial value of read_ptr is 0
+  reg [4:0] count = 5'b0; // Initial value of count is 0
+  
+  always @(posedge clk) begin
+    if (reset) begin
+      write_ptr <= 5'b0;
+      read_ptr <= 5'b0;
+      count <= 5'b0;
+      empty <= 1'b1;
+      full <= 1'b0;
+    end else begin
+      if (write_enable && ~full) begin
+        memory[write_ptr] <= data_in;
+        write_ptr <= write_ptr + 1;
+        count <= count + 1;
+        empty <= 1'b0;
+        if (count == 31)
+          full <= 1'b1;
+			 end
+      if (read_enable && ~empty) begin
+        data_out <= memory[read_ptr];
+        read_ptr <= read_ptr + 1;
+        count <= count - 1;
+        full <= 1'b0;
+        if (count == 1)
+          empty <= 1'b1;
+      end
+    end
+  end
+
+endmodule 
---
+++ Day_024/fifo_test.v
@@ -0,0 +1,80 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:38:20 07/17/2023
+// Design Name:   FIFO
+// Module Name:   fifo_test.v
+// Project Name:  FIFO
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FIFO
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module fifo_test_v;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg write_enable;
+	reg read_enable;
+	reg [15:0] data_in;
+
+	// Outputs
+	wire [15:0] data_out;
+	wire empty;
+	wire full;
+
+	// Instantiate the Unit Under Test (UUT)
+	FIFO uut (
+		.clk(clk), 
+		.reset(reset), 
+		.write_enable(write_enable), 
+		.read_enable(read_enable), 
+		.data_in(data_in), 
+		.data_out(data_out), 
+		.empty(empty), 
+		.full(full)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		reset = 0;
+		write_enable = 0;
+		read_enable = 0;
+		data_in = 0;
+
+		
+		#10
+		write_enable = 1;
+		data_in = 16'd12;
+		
+		#10
+		write_enable = 1;
+		data_in = 16'd13;
+		
+	
+		#10
+		write_enable = 0;
+		read_enable = 1;
+		
+		#20
+		$stop;
+		
+
+	end
+     always #5 clk=~clk; 
+endmodule
+
